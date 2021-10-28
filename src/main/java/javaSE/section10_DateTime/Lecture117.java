644753f62aa3f2c9d0c4b8e47079f55af07f3152
serhiyb86 <79664086+serhiyb86@users.noreply.github.com>
2021-10-28 18:39:59 +0300

    Java SE is completed

---
+++ src/main/java/javaSE/section10_DateTime/Lecture117.java
@@ -0,0 +1,42 @@
+package javaSE.section10_DateTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+
+public class Lecture117 {
+    public static void main(String[] args) {
+        LocalDate start = LocalDate.of(2017, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2017, Month.APRIL, 30);
+        Period period = Period.ofWeeks(2);
+
+        // start.toEpochDay(); // number of days since January 1 1970
+
+        cleanAnimalCage(start, end, period);
+
+        System.out.println();
+
+        System.out.println(start.plus(period));
+        System.out.println(LocalDateTime.now().plus(period));
+//        System.out.println(LocalTime.now().plus(period)); // unsupported unit day
+
+//        Period yearAndMonth = Period.ofYears(1).ofMonths(1);
+        Period yearAndMonth = Period.of(1, 1, 0);
+        System.out.println(yearAndMonth);
+    }
+
+    public static void cleanAnimalCage(LocalDate start, LocalDate end, Period period) {
+        while (start.isBefore(end)) {
+            System.out.println("Need to clean the cage on date= " + start);
+            start = start.plus(period);
+        }
+    }
+
+    public static void cleanAnimalCage(LocalDate start, LocalDate end) {
+        while (start.isBefore(end)) {
+            System.out.println("Need to clean the cage on date= " + start);
+            start = start.plusMonths(1); // adding 1 month
+        }
+    }
+}
